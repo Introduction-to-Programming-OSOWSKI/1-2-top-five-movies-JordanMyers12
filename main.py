c8f2df045ce7ba6cb82b234bc20f5fc6f667619e
JordanMyers12 <821028@mystma.org>
2022-09-16 08:40:35 -0500

    3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def topFiveMovies(a, b, c, d, e):
-    return a+ b+ c+ d+ e+ 
+    return a+ b+ c+ d+ e
   
 print(topFiveMovies("Morbius 2:It's Morbin time", "Shrek", 
-"The Emoji Movie","Acording to all Known laws of aviation","Fortnite The Musical" ))
+"The Emoji Movie",
+"Acording to all Known laws of aviation","Fortnite The Musical"))
